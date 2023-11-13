b91da0d8f3fe72d9f6c68be65cd59ac59b2c42d1
Grant <gkinghorn3@icloud.com>
2023-11-13 16:54:14 +0000

    btn component altered and keyframe conflict resolved

--- src/components/BtnMain/BtnMain.jsx
+++ src/components/BtnMain/BtnMain.jsx
@@ -3,7 +3,7 @@ import "./btn-main.scss";
 const BtnMain = ({ btnText, onClick, link, BtnType }) => {
   return (
     <>
-        <a className='btn-main uppercase' href={link}><span>{btnText}</span></a> 
+        <a className='btn-main uppercase' href={link} target="blank"><span>{btnText}</span></a> 
     </>
     
   );
