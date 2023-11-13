77de7a90dd270d940340a0e3c9b073f27d0ce201
grant kinghorn <gkinghorn3@icloud.com>
2023-11-13 21:58:40 +0000

    google maps api integration

--- src/components/FindUs/FindUs.jsx
+++ src/components/FindUs/FindUs.jsx
@@ -1,14 +1,39 @@
-import './find-us.scss';
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { useMemo } from "react";
+import "./find-us.scss";
 
 const FindUs = () => {
-  return (
-    <section className="find-us-container">
-      <div className="section-titles">
-        <h3>Find Us</h3>
-        <h2>- opening times -</h2>
-      </div>
-    </section>
-  );
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: "AIzaSyAkJTESuAIN3p16GxMlglzIpLl6BYUWLgY",
+    });
+
+    const center = useMemo(
+        () => ({ lat: 55.95341924477354, lng: -3.197819931188319 }),
+        []
+    );
+
+    
+
+    return (
+        <section className="find-us-container">
+            <div className="section-titles">
+                <h3>Find Us</h3>
+                <h2>- opening times -</h2>
+            </div>
+            {!isLoaded ? (
+                <h1>Loading...</h1>
+            ) : (
+                <GoogleMap
+                    mapContainerClassName="map-container"
+                    center={center}
+                    zoom={18}
+                >
+                    
+                    <Marker position={center} />
+                </GoogleMap>
+            )}
+        </section>
+    );
 };
 
 export default FindUs;
