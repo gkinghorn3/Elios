0da898fc0223948eb9877fe9fb415e9b5d751453
grant kinghorn <gkinghorn3@icloud.com>
2025-02-19 18:34:31 +0000

    update booking link

--- src/components/Header/Navigation/Nav.jsx
+++ src/components/Header/Navigation/Nav.jsx
@@ -1,27 +1,30 @@
-import {Link} from 'react-scroll';
+import { Link } from "react-scroll";
 
-
-import './nav.scss'
-
+import "./nav.scss";
 
 const Nav = () => {
-
-    return (
-        <nav>
-            <div className='nav-link-container'>
-                <a href='https://elios.covergenie.uk/booking/elios-edinburgh' target='blank'>BOOK</a>
-            </div>
-            <div className='nav-link-container'>
-                <Link to='menu' smooth={true} duration={1000}>MENU</Link>
-            </div>
-            <div className='nav-link-container'>
-                <Link to='find-us' smooth={true} duration={1000}>FIND US</Link>
-            </div >
-            
-            
-            
-        </nav>
-    )
-}
+  return (
+    <nav>
+      <div className="nav-link-container">
+        <a
+          href="https://bookings.designmynight.com/book?venue_id=6630f8138ce9bb509a3fedf8&source=partner"
+          target="blank"
+        >
+          BOOK
+        </a>
+      </div>
+      <div className="nav-link-container">
+        <Link to="menu" smooth={true} duration={1000}>
+          MENU
+        </Link>
+      </div>
+      <div className="nav-link-container">
+        <Link to="find-us" smooth={true} duration={1000}>
+          FIND US
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
